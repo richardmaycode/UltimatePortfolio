b6c82e2d0043b1c8603f8a4f077fe1c39192e9f4
Richard Wise <richard.wise@hey.com>
2023-02-21 10:09:40 -0500

    Init coredata

--- UltimatePortfolio/UltimatePortfolioApp.swift
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 @main
 struct UltimatePortfolioApp: App {
+    
+    @State var dataController = DataController()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
         }
     }
 }
