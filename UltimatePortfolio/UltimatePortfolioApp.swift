d375e50ff9ed07b461feafc5ea545104085f5d35
Richard Wise <richard.wise@hey.com>
2023-02-21 08:21:24 -0500

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Richard Wise on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
